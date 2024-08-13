1b0dbd098a7aaad00388fd23af4fb9b69669bb17
Piotr Skiba <pskiba37@gmail.com>
2024-08-13 21:36:22 +0200

    fix text color in night mode

--- app/src/main/java/pl/piotrskiba/exchangerates/ratelist/view/RateListElement.kt
+++ app/src/main/java/pl/piotrskiba/exchangerates/ratelist/view/RateListElement.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -19,32 +20,34 @@ import pl.piotrskiba.exchangerates.ratelist.model.Table
 
 @Composable
 fun RateListElement(rate: Rate, onClick: (Rate) -> Unit) {
-    Column(modifier = Modifier
-        .padding(horizontal = 8.dp)
-        .clickable { onClick(rate) }) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-        ) {
-            Column(Modifier.weight(1f)) {
+    Surface {
+        Column(modifier = Modifier
+            .padding(horizontal = 8.dp)
+            .clickable { onClick(rate) }) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Column(Modifier.weight(1f)) {
+                    Text(
+                        text = rate.currency,
+                        style = MaterialTheme.typography.titleLarge,
+                    )
+                    Text(
+                        text = rate.code,
+                        style = MaterialTheme.typography.bodyLarge,
+                    )
+                }
                 Text(
-                    text = rate.currency,
-                    style = MaterialTheme.typography.titleLarge,
-                )
-                Text(
-                    text = rate.code,
+                    text = rate.mid,
                     style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.align(Alignment.CenterVertically)
                 )
             }
-            Text(
-                text = rate.mid,
-                style = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier.align(Alignment.CenterVertically)
-            )
+            HorizontalDivider()
         }
-        HorizontalDivider()
     }
 }
 
